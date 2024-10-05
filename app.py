8bc312233dcbec7145b147ec47ef9392ad10da1a
Gabriel Ribeiro Filice Chayb <95705080+gabrielchayb@users.noreply.github.com>
2024-10-05 19:40:19 -0300

    verification commit

--- app.py
+++ app.py
@@ -7,35 +7,42 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-# second piece of text
-@app.route('/home1')
-def home1():
-    return render_template('home1.html')
+# scene in mars , second piece of text
+@app.route('/mars')
+def mars():
+    return render_template('mars.html')
 
-# third piece of text
+# second piece of text moon
 @app.route('/home2')
 def home2():
     return render_template('home2.html')
 
-# from home1, redirect to posreceivied 
+# third piece of text moon
+@app.route('/home1')
+def home1():
+    return render_template('home1.html')
+
+# from home1, redirect to poscode , that is the video page 
 @app.route('/receivingcode')
 def receivingcode():
     return render_template('receivingcode.html')
 
-# from receivingcode, redirect to explaining
+# from receivingcode, redirect to choose mission
 @app.route('/poscode')
 def poscode():
     return render_template('poscode.html')
+
+# from explaining, redirect to : apollo OR mars insight lander
+@app.route('/choosemission')
+def choosemission():
+    return render_template('choosemission.html')
 
 # from posreceivied, redirect to choosemission
 @app.route('/explaining')
 def explaining():
     return render_template('explaining.html')
 
-# from explaining, redirect to : apollo OR mars insight lander
-@app.route('/choosemission')
-def choosemission():
-    return render_template('choosemission.html')
+
 
 
 
