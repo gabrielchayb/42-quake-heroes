ab7612337456ace22b56e4359c051d8818ad42db
Gabriel Ribeiro Filice Chayb <95705080+gabrielchayb@users.noreply.github.com>
2024-10-05 14:41:36 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# home, index, first page , redirect to receivingcode
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/home1')
+def home1():
+    return render_template('home1.html')
+
+# from home, redirect to posreceivied 
+@app.route('/receivingcode')
+def receivingcode():
+    return render_template('receivingcode.html')
+
+# from receivingcode, redirect to explaining
+@app.route('/posreceivied')
+def posreceivied():
+    return render_template('posreceivied.html')
+
+# from posreceivied, redirect to choosemission
+@app.route('/explaining')
+def explaining():
+    return render_template('explaining.html')
+
+# from explaining, redirect to : apollo OR mars insight lander
+@app.route('/choosemission')
+def choosemission():
+    return render_template('choosemission.html')
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
